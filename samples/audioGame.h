dc16ce7f419740c0f62c03c716015ce4d6fb7d7f
brenden-t-r <brenden.romanowski@gmail.com>
2025-02-06 21:57:06 -0600

    feat: audio

--- samples/audioGame.h
+++ samples/audioGame.h
@@ -9,8 +9,11 @@ class AudioGame : public Game {
 public:
     using Game::Game;
     ~AudioGame() override {
-        delete audioSound;
+        delete kick;
+        delete snare;
+        delete hat;
         delete triangle;
+        delete ride;
         delete audioWrapper;
     };
 
@@ -19,31 +22,82 @@ public:
         audioWrapper->Init();
         //audioWrapper->Play(R"(C:\Users\Brenden\Desktop\2025Song9.mp3)");
 
-        audioSound = new AudioSoundWrapper();
-        audioSound->Init(R"(C:\Users\Brenden\Desktop\Kick.wav)");
-        audioSound->Play();
+        kick = new AudioSoundWrapper();
+        kick->Init(R"(C:\Users\Brenden\Desktop\Kick.wav)");
+        snare = new AudioSoundWrapper();
+        snare->Init(R"(C:\Users\Brenden\Desktop\Snare.wav)");
+        hat = new AudioSoundWrapper();
+        hat->Init(R"(C:\Users\Brenden\Desktop\Hat.wav)");
+        ride = new AudioSoundWrapper();
+        ride->Init(R"(C:\Users\Brenden\Desktop\Ride.wav)");
+        ma_sound_set_volume(&hat->sound, 0.5f);
 
         platform->LoadShaders();
         triangle = platform->CreateTriangle();
     }
 
-    int count = 0;
+    int countKick = 0;
+    int countSnare = 0;
+    int countHat = 0;
+    int countRide = 0;
+    int speed = 12;
+    int bars = 1;
+    int barCount = 0;
     void Update() override {
         printf(".");
         triangle->Update();
 
-        count+=1;
-        if (count == 25) {
-            count = 0;
-            audioSound->Reset();
-            audioSound->Play();
+        barCount++;
+        if (barCount == speed*4) {
+            barCount = 0;
+            bars++;
         }
+        if (bars == 1) {
+            //bars = 0;
+        }
+
+        if (bars <= 16) {
+            if (countHat == speed*2 || countHat == 0) {
+                countHat = 0;
+                hat->Reset();
+                hat->Play();
+            }
+        } else {
+            if (countHat >= speed || countHat == 0) {
+                countHat = 0;
+                hat->Reset();
+                hat->Play();
+            }
+        }
+
+        if (countKick == speed*4|| countKick == 0) {
+            countKick = 0;
+            kick->Reset();
+            kick->Play();
+        }
+        if (countSnare == speed*4) {
+            countSnare = -speed*4;
+            snare->Reset();
+            snare->Play();
+        }
+        if (countRide == speed*32 || countRide == 0) {
+            countRide = 0;
+            ride->Reset();
+            ride->Play();
+        }
+        countKick++;
+        countSnare++;
+        countHat++;
+        countRide++;
     }
 
 private:
     GameObject* triangle = nullptr;
     AudioWrapper* audioWrapper = nullptr;
-    AudioSoundWrapper* audioSound = nullptr;
+    AudioSoundWrapper* kick = nullptr;
+    AudioSoundWrapper* snare = nullptr;
+    AudioSoundWrapper* hat = nullptr;
+    AudioSoundWrapper* ride = nullptr;
 };
 
 #endif //GAMEENGINE_AUDIOGAME_H
