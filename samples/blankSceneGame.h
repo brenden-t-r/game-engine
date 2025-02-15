69de1ee6ba15bf26d29915ff37e6b6c0e3e97936
brenden-t-r <brenden.romanowski@gmail.com>
2025-02-14 19:24:25 -0600

    Scene

--- samples/blankSceneGame.h
+++ samples/blankSceneGame.h
@@ -5,17 +5,45 @@
 
 enum scenes {
     SCENE_MAIN,
+    SCENE_2,
 };
 
 class BlankScene : public Scene {
     using Scene::Scene;
-    void Start() override {}
-    void Update() override {}
+    ~BlankScene() {
+        delete gameObject;
+    }
+    void Start() override {
+        gameObject = platform->CreateTriangle();
+    }
+    void Update() override {
+        counter+=1;
+        if (counter > 50) nextScene = SCENE_2;
+        gameObject->Update();
+    }
+    GameObject* gameObject = nullptr;
+    int counter = 0;
+};
+
+class BlankScene2 : public Scene {
+    using Scene::Scene;
+    void Start() override {
+        gameObject = platform->CreateTriangle();
+        gameObject->transform.pos.x += 0.5f;
+    }
+    void Update() override {
+        counter+=1;
+        if (counter > 50) nextScene = SCENE_MAIN;
+        gameObject->Update();
+    }
+    GameObject* gameObject = nullptr;
+    int counter = 0;
 };
 
 static Scene* GetSceneFn(Platform* platform, int sceneToLoad) {
     switch (sceneToLoad) {
         case SCENE_MAIN: return new BlankScene(platform);
+        case SCENE_2: return new BlankScene2(platform);
         default: return nullptr;
     }
 }
@@ -29,6 +57,7 @@ public:
     };
 
     void Start() override {
+        platform->LoadShaders();
         scene = new BlankScene(platform);
         scene->Start();
     }
