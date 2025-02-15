e44e584d67815f3d2812965c12b45c3a09dfdca4
brenden-t-r <brenden.romanowski@gmail.com>
2025-02-14 20:22:09 -0600

    Scene

--- main.cpp
+++ main.cpp
@@ -39,7 +39,6 @@ void GameUpdateFn(void* context) {
     ((Game*)(context))->Update();
 }
 
-
 // Common logic for the application
 int RealMain(Platform* platform) {
     printf("Hello from PlatformMain!\n");
