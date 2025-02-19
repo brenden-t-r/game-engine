91001f4b3bff1cae9f277099668eca0efbb40952
brenden-t-r <brenden.romanowski@gmail.com>
2025-02-18 21:22:28 -0600

    Font

--- platform/directx/directx.h
+++ platform/directx/directx.h
@@ -599,7 +599,7 @@ private:
     void InitPipeline() {
         // Create a sampler state
         D3D11_SAMPLER_DESC samplerDesc = {};
-        samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+        samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
         samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_CLAMP;
         samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_CLAMP;
         samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_CLAMP;
