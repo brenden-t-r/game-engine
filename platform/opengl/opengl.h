02d5e6642b5b5871d6837ab22dd2a70d507189f4
brenden-t-r <brenden.romanowski@gmail.com>
2025-03-04 20:41:33 -0600

    opengl cleanup

--- platform/opengl/opengl.h
+++ platform/opengl/opengl.h
@@ -14,9 +14,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-GLuint compileShader(const char* source, GLenum type);
-GLuint loadTexture(const char* path);
-
+//region Input Helpers / Callbacks
 static int GetGLFWKey(KeyCode keyCode) {
     switch (keyCode) {
         case KeyCode::Up:       return GLFW_KEY_UP;
@@ -55,7 +53,6 @@ void static(*keyUpCallback)(KeyCode, void*);
 static void* keyCallbackContext;
 void static(*mouseUpCallback)(MouseButton, void*);
 static void* mouseCallbackContext;
-
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 {
     if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
@@ -76,6 +73,7 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
+//endregion
 
 class PlatformOpenGL : public Platform {
 public:
@@ -83,7 +81,6 @@ public:
     ~PlatformOpenGL() override = default;
 
     void Init() override {
-        /* Initialize the library */
         if (!glfwInit()) {
             fprintf(stderr, "Failed to initialize GLFW\n");
             return;
@@ -128,57 +125,6 @@ public:
         setupVAOs();
     }
 
-    void setupVAOs() {
-        // Triangle Data
-        float triangleVertices[] = {
-                // Positions
-                0.0f,  0.5f, 0.0f, // Top
-                -0.5f, -0.5f, 0.0f, // Left
-                0.5f, -0.5f, 0.0f  // Right
-        };
-
-        // Quad Data
-        float quadVertices[] = {
-                // Positions      // Texture Coords
-                -1.0f,  1.0f, 0.0f,  0.0f, 1.0f, // Top-left
-                1.0f,  1.0f, 0.0f,  1.0f, 1.0f, // Top-right
-                1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // Bottom-right
-                -1.0f, -1.0f, 0.0f,  0.0f, 0.0f  // Bottom-left
-        };
-        unsigned int quadIndices[] = {
-                0, 1, 2, // First Triangle
-                0, 2, 3  // Second Triangle
-        };
-
-        // VAO and VBO for Triangle
-        glGenVertexArrays(1, &triangleVAO);
-        glGenBuffers(1, &triangleVBO);
-
-        // VAO, VBO, and EBO for Quad
-        unsigned int quadEBO;
-        glGenVertexArrays(1, &quadVAO);
-        glGenBuffers(1, &quadVBO);
-        glGenBuffers(1, &quadEBO);
-
-        glBindVertexArray(triangleVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0); // Position attribute
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-        glBindVertexArray(0); // Unbind VAO
-
-        glBindVertexArray(quadVAO);
-        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
-        glEnableVertexAttribArray(0); // Position attribute
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(1); // Texture coordinate attribute
-        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-        glBindVertexArray(0); // Unbind VAO
-    }
-
     void LoadShaders() override {
         triangleShader = loadShaderProgram(
                 "assets/shaders/SimpleVertexShader.vertexshader",
@@ -210,6 +156,7 @@ public:
         );
     }
 
+    //region Input Handling
     bool IsKeyPressed(KeyCode key) override {
         auto keyCode = GetGLFWKey(key);
         int state = glfwGetKey(window, keyCode);
@@ -220,6 +167,14 @@ public:
         int state = glfwGetMouseButton(window, btn);
         return state == GLFW_PRESS;
     }
+    void SetKeyReleasedCallback(void (*func)(KeyCode, void*), void* context) override {
+        keyUpCallback = func;
+        keyCallbackContext = context;
+    }
+    void SetMouseReleasedCallback(void (*func)(MouseButton, void*), void* context) override {
+        mouseUpCallback = func;
+        mouseCallbackContext = context;
+    }
     vector3 GetMousePos() override {
         double cursorX, cursorY;
         int width, height;
@@ -229,31 +184,9 @@ public:
         float ndcY = 1.0f - (2.0f * (float)cursorY) / (float)height;  // Flip Y axis
         return {ndcX, ndcY, 0};
     }
+    //endregion
 
-
-    void SetKeyReleasedCallback(void (*func)(KeyCode, void*), void* context) override {
-        keyUpCallback = func;
-        keyCallbackContext = context;
-    }
-    void SetMouseReleasedCallback(void (*func)(MouseButton, void*), void* context) override {
-        mouseUpCallback = func;
-        mouseCallbackContext = context;
-    }
-
-    void Shutdown() override{
-        glDisableVertexAttribArray(0);
-        glDeleteVertexArrays(1, &triangleVAO);
-        glDeleteProgram(textureShader);
-
-        // Close OpenGL window and terminate GLFW
-        glfwTerminate();
-    }
-
-    GameObject* CreateGameObject() override {
-        auto gameObject = new GameObject();
-        return gameObject;
-    }
-
+    //region GameObjects
     class TriangleGL : public Triangle{
 
     public:
@@ -349,6 +282,16 @@ public:
         gameObject->vertexBufferObject = quadVBO;
         return gameObject;
     }
+    //endregion
+
+    void Shutdown() override{
+        glDisableVertexAttribArray(0);
+        glDeleteVertexArrays(1, &triangleVAO);
+        glDeleteProgram(textureShader);
+
+        // Close OpenGL window and terminate GLFW
+        glfwTerminate();
+    }
 
 private:
     GLFWwindow *window = nullptr;
@@ -359,31 +302,58 @@ private:
     GLuint quadVBO = 0;
     GLuint textureShader = 0;
 
-    static char* getFileContent(const char* fileName)
-    {
-        FILE *fp;
-        long size = 0;
-        char* shaderContent;
+    void setupVAOs() {
+        // Triangle Data
+        float triangleVertices[] = {
+                // Positions
+                0.0f,  0.5f, 0.0f, // Top
+                -0.5f, -0.5f, 0.0f, // Left
+                0.5f, -0.5f, 0.0f  // Right
+        };
 
-        /* Read File to get size */
-        fp = fopen(fileName, "rb");
-        if(fp == nullptr) {
-            printf("Error reading %s\n", fileName);
-            return "";
-        }
-        fseek(fp, 0L, SEEK_END);
-        size = ftell(fp)+1;
-        fclose(fp);
+        // Quad Data
+        float quadVertices[] = {
+                // Positions      // Texture Coords
+                -1.0f,  1.0f, 0.0f,  0.0f, 1.0f, // Top-left
+                1.0f,  1.0f, 0.0f,  1.0f, 1.0f, // Top-right
+                1.0f, -1.0f, 0.0f,  1.0f, 0.0f, // Bottom-right
+                -1.0f, -1.0f, 0.0f,  0.0f, 0.0f  // Bottom-left
+        };
+        unsigned int quadIndices[] = {
+                0, 1, 2, // First Triangle
+                0, 2, 3  // Second Triangle
+        };
 
-        /* Read File for Content */
-        fp = fopen(fileName, "r");
-        shaderContent = static_cast<char *>(memset(malloc(size), '\0', size));
-        fread(shaderContent, 1, size-1, fp);
-        fclose(fp);
+        // VAO and VBO for Triangle
+        glGenVertexArrays(1, &triangleVAO);
+        glGenBuffers(1, &triangleVBO);
 
-        return shaderContent;
+        // VAO, VBO, and EBO for Quad
+        unsigned int quadEBO;
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glGenBuffers(1, &quadEBO);
+
+        glBindVertexArray(triangleVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, triangleVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0); // Position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glBindVertexArray(0); // Unbind VAO
+
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0); // Position attribute
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1); // Texture coordinate attribute
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+        glBindVertexArray(0); // Unbind VAO
     }
 
+    //region Shaders
     static GLuint loadShaderProgram(const char* vertex, const char* fragment) {
         auto vertexSource = getFileContent(vertex);
         auto fragmentSource = getFileContent(fragment);
@@ -415,73 +385,98 @@ private:
 
         return program;
     }
-};
 
-// Shader compilation helper function
-GLuint compileShader(const char* source, GLenum type) {
-    GLuint shader = glCreateShader(type);
-    glShaderSource(shader, 1, &source, NULL);
-    glCompileShader(shader);
+    static char* getFileContent(const char* fileName)
+    {
+        FILE *fp;
+        long size = 0;
+        char* shaderContent;
 
-    // Check for compilation errors
-    GLint success;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        char infoLog[512];
-        glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cerr << "Shader compilation failed: " << infoLog << std::endl;
+        /* Read File to get size */
+        fp = fopen(fileName, "rb");
+        if(fp == nullptr) {
+            printf("Error reading %s\n", fileName);
+            return "";
+        }
+        fseek(fp, 0L, SEEK_END);
+        size = ftell(fp)+1;
+        fclose(fp);
+
+        /* Read File for Content */
+        fp = fopen(fileName, "r");
+        shaderContent = static_cast<char *>(memset(malloc(size), '\0', size));
+        fread(shaderContent, 1, size-1, fp);
+        fclose(fp);
+
+        return shaderContent;
     }
 
-    return shader;
-}
+    static GLuint compileShader(const char* source, GLenum type) {
+        GLuint shader = glCreateShader(type);
+        glShaderSource(shader, 1, &source, NULL);
+        glCompileShader(shader);
 
-GLuint loadTexture(const char* path) {
-    GLuint textureID;
-    glGenTextures(1, &textureID);
-
-    // Load image data
-    int width, height, channels;
-    stbi_set_flip_vertically_on_load(true); // OpenGL expects texture coordinates to start from bottom-left
-    unsigned char* data = stbi_load(path, &width, &height, &channels, 0);
-
-    if (data) {
-        GLenum format;
-        switch (channels) {
-            case 1:
-                format = GL_RED;
-                break;
-            case 3:
-                format = GL_RGB;
-                break;
-            case 4:
-                format = GL_RGBA;
-                break;
-            default:
-                format = GL_RGBA;
-                break;
+        // Check for compilation errors
+        GLint success;
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            char infoLog[512];
+            glGetShaderInfoLog(shader, 512, NULL, infoLog);
+            std::cerr << "Shader compilation failed: " << infoLog << std::endl;
         }
 
-        // Bind and set texture parameters
-        glBindTexture(GL_TEXTURE_2D, textureID);
-        glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
-
-        // Set texture parameters
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        // Free image data
-        stbi_image_free(data);
-    } else {
-        std::cerr << "Failed to load texture: " << path << std::endl;
-        stbi_image_free(data);
-        return 0;
+        return shader;
     }
+    //endregion
 
-    return textureID;
-}
+    static GLuint loadTexture(const char* path) {
+        GLuint textureID;
+        glGenTextures(1, &textureID);
+
+        // Load image data
+        int width, height, channels;
+        stbi_set_flip_vertically_on_load(true); // OpenGL expects texture coordinates to start from bottom-left
+        unsigned char* data = stbi_load(path, &width, &height, &channels, 0);
+
+        if (data) {
+            GLenum format;
+            switch (channels) {
+                case 1:
+                    format = GL_RED;
+                    break;
+                case 3:
+                    format = GL_RGB;
+                    break;
+                case 4:
+                    format = GL_RGBA;
+                    break;
+                default:
+                    format = GL_RGBA;
+                    break;
+            }
+
+            // Bind and set texture parameters
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+
+            // Set texture parameters
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            // Free image data
+            stbi_image_free(data);
+        } else {
+            std::cerr << "Failed to load texture: " << path << std::endl;
+            stbi_image_free(data);
+            return 0;
+        }
+
+        return textureID;
+    }
+};
 
 // Entrypoint
 #if PLATFORM_WINDOWS
