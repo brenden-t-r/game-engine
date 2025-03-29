9ae7818af7cfcca499f53d024a6c62317bda817e
Brenden Romanowski <brenden.romanowski@gmail.com>
2025-03-29 15:15:19 -0500

    macos triangles

--- platform/metal/main.mm
+++ platform/metal/main.mm
@@ -34,14 +34,129 @@ fragment float4 fragment_main() {
 )";
 //endregion
 
-//region: MetalView
+//region: MetalView/MetalAppDelegate declarations
 @interface MetalView : MTKView <MTKViewDelegate>
 @property (nonatomic, strong) id<MTLCommandQueue> commandQueue;
-@property (nonatomic, strong) id<MTLRenderPipelineState> pipelineState;
-@property (nonatomic, strong) id<MTLBuffer> vertexBuffer;
+@property (nonatomic, strong) id<MTLRenderPipelineState> trianglePSO;
 @property (nonatomic, assign) void *funcContext;
 @property (nonatomic, assign) void (*func)(void *);
 @end
+@interface MetalAppDelegate : NSObject <NSApplicationDelegate>
+@property (strong, nonatomic) NSWindow *window;
+@property (strong, nonatomic) MetalView *metalView;
+@property (strong, nonatomic) id<MTLDevice> device;
+@end
+//endregion
+
+//region: Game Objects
+class TriangleMetal : public Triangle {
+public:
+    ~TriangleMetal() {
+        [vertexBuffer release];
+    }
+    TriangleMetal(
+            id <MTLDevice> metalDevice,
+            id <MTLRenderPipelineState> metalRenderPSO
+    ){
+        this->metalDevice = metalDevice;
+        this->metalRenderPSO = metalRenderPSO;
+        static const float vertices[] = {
+                vertex1.x, vertex1.y, 0.0f, 1.0f,  // Top vertex
+                vertex2.x, vertex2.y, 0.0f, 1.0f,  // Bottom left vertex
+                vertex3.x, vertex3.y, 0.0f, 1.0f   // Bottom right vertex
+        };
+        vertexBuffer = [metalDevice newBufferWithBytes:&vertices length:sizeof(vertices) options:MTLResourceStorageModeShared];
+    }
+
+    void Update() override {
+        Triangle::Update();
+        float vertices[] = {
+                vertex1.x, vertex1.y, 0.0f, 1.0f,  // Top vertex
+                vertex2.x, vertex2.y, 0.0f, 1.0f,  // Bottom left vertex
+                vertex3.x, vertex3.y, 0.0f, 1.0f   // Bottom right vertex
+        };
+        memcpy([vertexBuffer contents], vertices, sizeof(vertices));
+        [renderCommandEncoder setRenderPipelineState:metalRenderPSO];
+        [renderCommandEncoder setVertexBuffer:vertexBuffer offset:0 atIndex:0];
+        [renderCommandEncoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3];
+    }
+
+    void SetRenderCommandEncoder(id<MTLRenderCommandEncoder> commandEncoder) {
+        this->renderCommandEncoder = commandEncoder;
+    }
+
+private:
+    id<MTLDevice> metalDevice;
+    id<MTLBuffer> vertexBuffer;
+    id<MTLRenderPipelineState> metalRenderPSO;
+    id<MTLRenderCommandEncoder> renderCommandEncoder;
+};
+//endregion
+
+//region: PlatformMetal
+static bool Running = false;
+std::vector<TriangleMetal*> triangles = std::vector<TriangleMetal*>();
+class PlatformMetal : public Platform {
+public:
+    void Init() override {
+        printf("Hi from Init\n");
+    }
+    void Run(void (*func)(void*), void* context) override {
+        metalAppDelegate.metalView.func = func;
+        metalAppDelegate.metalView.funcContext = context;
+        Running = true;
+        while (Running) {
+            [[NSRunLoop currentRunLoop] runMode:NSDefaultRunLoopMode beforeDate:[NSDate distantFuture]];
+        }
+        printf("Run end");
+    }
+    void LoadShaders() override {
+
+    }
+    GameObject* CreateGameObject() override { return new GameObject(); };
+    GameObject* CreateTriangle() override {
+        auto gameObject = new TriangleMetal(metalAppDelegate.device, metalAppDelegate.metalView.trianglePSO);
+        triangles.push_back(gameObject);
+        return gameObject;
+    }
+    Sprite* CreateSprite(const char* path) override {
+        return nullptr;
+    }
+    bool IsKeyPressed(KeyCode key) override {
+        return false;
+    }
+    bool IsMousePressed(MouseButton button) override {
+        return false;
+    }
+    bool IsGamepadButtonPressed(GamepadButton button) override {
+        return false;
+    }
+    void SetKeyReleasedCallback(void (*func)(KeyCode, void*), void* context) override {
+        return;
+    }
+    void SetMouseReleasedCallback(void (*func)(MouseButton, void*), void* context) override {
+        return;
+    }
+    void SetGamepadReleasedCallback(void (*func)(GamepadButton, void*), void* context) override {
+        return;
+    }
+    virtual vec3 GetMousePos() override { return {}; }
+    void Shutdown() override {
+        Running = false;
+    }
+
+    MetalAppDelegate* metalAppDelegate;
+};
+
+int RealMain(Platform* platform);
+static void RealMainMetal(MetalAppDelegate* app, MetalView* view) {
+    auto platform = new PlatformMetal();
+    platform->metalAppDelegate = app;
+    RealMain(platform);
+}
+//endregion
+
+//region: MetalView
 @implementation MetalView
 - (instancetype)initWithFrame:(NSRect)frame {
     self = [super initWithFrame:frame];
@@ -79,8 +194,8 @@ fragment float4 fragment_main() {
     pipelineDesc.fragmentFunction = fragmentFunction;
     pipelineDesc.colorAttachments[0].pixelFormat = self.colorPixelFormat;
 
-    self.pipelineState = [self.device newRenderPipelineStateWithDescriptor:pipelineDesc error:&error];
-    if (!self.pipelineState) {
+    self.trianglePSO = [self.device newRenderPipelineStateWithDescriptor:pipelineDesc error:&error];
+    if (!self.trianglePSO) {
         NSLog(@"Pipeline creation error: %@", error.localizedDescription);
     }
 }
@@ -93,24 +208,20 @@ fragment float4 fragment_main() {
 
     passDescriptor.colorAttachments[0].clearColor = MTLClearColorMake(0.4, 0.4, 0.8, 1.0); // Black background
     passDescriptor.colorAttachments[0].loadAction = MTLLoadActionClear;
+    passDescriptor.colorAttachments[0].storeAction = MTLStoreActionStore;
 
-    static const float vertices[] = {
-            -0.5, -0.5, 0.0f, 1.0f,  // Top vertex
-            0.5f, -0.5f, 0.0f, 1.0f,  // Bottom left vertex
-            0.0f, 0.5f, 0.0f, 1.0f   // Bottom right vertex
-    };
-    self.vertexBuffer = [self.device newBufferWithBytes:&vertices length:sizeof(vertices) options:MTLResourceStorageModeShared];
+    id<MTLRenderCommandEncoder> renderCommandEncoder = [commandBuffer renderCommandEncoderWithDescriptor:passDescriptor];
 
-    id<MTLRenderCommandEncoder> encoder = [commandBuffer renderCommandEncoderWithDescriptor:passDescriptor];
-    [encoder setRenderPipelineState:self.pipelineState];
-    [encoder setVertexBuffer:self.vertexBuffer offset:0 atIndex:0];
-    [encoder drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:3];
-    [encoder endEncoding];
+    for (TriangleMetal* gameObject : triangles) {
+        gameObject->SetRenderCommandEncoder(renderCommandEncoder);
+    }
 
     self.func(self.funcContext);
 
+    [renderCommandEncoder endEncoding];
     [commandBuffer presentDrawable:view.currentDrawable];
     [commandBuffer commit];
+    [commandBuffer waitUntilCompleted];
 }
 
 - (void)mtkView:(nonnull MTKView *)view drawableSizeWillChange:(CGSize)size __attribute__((swift_attr("@UIActor"))) {
@@ -120,13 +231,9 @@ fragment float4 fragment_main() {
 //endregion
 
 //region: AppDelegate
-@interface MetalAppDelegate : NSObject <NSApplicationDelegate>
-@property (strong, nonatomic) NSWindow *window;
-@property (nonatomic, assign) void *funcContext;
-@property (nonatomic, assign) void (*func)(void *);
-@end
 @implementation MetalAppDelegate
 - (void)applicationDidFinishLaunching:(NSNotification *)notification {
+    NSLog(@"applicationDidFinishLaunching");
     NSRect frame = NSMakeRect(100, 100, 800, 600);
     self.window = [[NSWindow alloc] initWithContentRect:frame
                                               styleMask:(NSWindowStyleMaskTitled | NSWindowStyleMaskClosable |
@@ -141,73 +248,23 @@ fragment float4 fragment_main() {
 
     MetalView *metalView = [[MetalView alloc] initWithFrame:frame];
     self.window.contentView = metalView;
-    metalView = metalView;
-    metalView.func = self.func;
-    metalView.funcContext = self.funcContext;
+    self.metalView = metalView;
+    self.device = metalView.device;
 
-    [self.window makeKeyAndOrderFront:nil];
+    RealMainMetal(self, self.metalView);
 }
 @end
-
-class PlatformMetal : public Platform {
-public:
-    void Init() override {
-        printf("Hi from Init\n");
-    }
-    void Run(void (*func)(void*), void* context) override {
-        printf("Hi from Run\n");
-        metalAppDelegate.func = func;
-        metalAppDelegate.funcContext = context;
-        [app run];
-    }
-    void LoadShaders() override {
-
-    }
-    GameObject* CreateGameObject() override { return new GameObject(); };
-    GameObject* CreateTriangle() override {
-            return nullptr;
-    }
-    Sprite* CreateSprite(const char* path) override {
-        return nullptr;
-    }
-    bool IsKeyPressed(KeyCode key) override {
-        return false;
-    }
-    bool IsMousePressed(MouseButton button) override {
-        return false;
-    }
-    bool IsGamepadButtonPressed(GamepadButton button) override {
-        return false;
-    }
-    void SetKeyReleasedCallback(void (*func)(KeyCode, void*), void* context) override {
-        return;
-    }
-    void SetMouseReleasedCallback(void (*func)(MouseButton, void*), void* context) override {
-        return;
-    }
-    void SetGamepadReleasedCallback(void (*func)(GamepadButton, void*), void* context) override {
-        return;
-    }
-    virtual vec3 GetMousePos() override { return {}; }
-    void Shutdown() override {}
-
-    NSApplication* app;
-    MetalAppDelegate* metalAppDelegate;
-};
 //endregion
 
 //region: Main
-int RealMain(Platform* platform);
 int main(int argc, const char * argv[]) {
     printf("Hello world\n");
     @autoreleasepool {
         NSApplication *app = [NSApplication sharedApplication];
         MetalAppDelegate *delegate = [[MetalAppDelegate alloc] init];
         [app setDelegate:delegate];
-        auto platform = new PlatformMetal();
-        platform->app = app;
-        platform->metalAppDelegate = delegate;
-        RealMain(platform);
+        [app finishLaunching];
+        [app run];
     }
     return 0;
 }
